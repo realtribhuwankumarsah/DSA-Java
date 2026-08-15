f1c280d2105e10b84b6c9002486c8e9a7dfe5fe2
realtribhuwankumar <realtribhuwankumar@gmail.com>
2026-08-16 02:35:24 +0530

    Updated

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,4 +1,4 @@
-package Arrays;
+package BinarySearch;
 
 public class BinarySearch {
 
--- Revision/Revision.java
+++ Revision/Revision.java
@@ -51,6 +51,35 @@ public class Revision {
 
 
     }
+
+
+
+
+
+
+
+
+
+    public static void PairsInArray(int arr[]) {
+        int totalPairs = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                System.out.print("{"+arr[i]+"," +arr[j] +"}");
+                totalPairs++;
+                
+            }
+            System.out.println();
+            
+        }
+        System.out.println(totalPairs);
+    }
+
+
+    
+
+
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr =ArrayCretion(sc);
@@ -59,6 +88,7 @@ public class Revision {
         
         int result = BinarySearch(arr,sc);
         System.out.println(result);
+        PairsInArray(arr);
 
         
 
