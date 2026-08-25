5bbc3a6872af5e14207e0eb8f56f88f6cebf9868
realtribhuwankumar <realtribhuwankumar@gmail.com>
2026-08-25 11:24:16 +0530

    SelectionSorc Complete

---
+++ Arrays/selectionSort.java
@@ -0,0 +1,31 @@
+public class selectionSort {
+    public static void main(String[] args) {
+    int arr[] = {5,4,1,3,2};
+    int MinElement = Integer.MAX_VALUE;
+    
+    //outerloop=Turns/Paases
+    for (int i = 0; i < arr.length-1; i++) {
+        int minValueIndex = i;
+        //InnerLoop = Seaching for Minimum Value
+        for (int j = i+1; j < arr.length; j++) {
+        if(arr[minValueIndex]>arr[j]) {
+            minValueIndex=j;
+
+        }
+       
+
+            
+        }
+         //Swapping the Values
+        int temp = arr[i];
+        arr[i]=arr[minValueIndex];
+        arr[minValueIndex] = temp;
+        
+    }
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]+" ");
+        
+    }
+    }
+    
+}
