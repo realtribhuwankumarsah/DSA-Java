0d8e33ff96b80abf6a4cc6638d431ff83c2a4f2b
realtribhuwankumar <realtribhuwankumar@gmail.com>
2026-08-26 06:35:11 +0530

    counting sort

--- Arrays/sorting.java
+++ Arrays/sorting.java
@@ -87,11 +87,42 @@ public class sorting {
             
         }
     }
+
+    public static void countingSort (int arr[]) {
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            max  = Math.max(max, arr[i]);
+            
+        }
+
+        int count[] = new int[max+1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+            
+        }
+        int j =0;
+        for (int i = 0; i < count.length; i++) {
+            while(count[i]>0) {
+                arr[j]=i;
+                j++;
+                count[i]--;
+            
+        }
+            
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+            
+        }
+    }
     public static void main(String[] args) {
-        int arr[] = {5,9,8,6,3,7,9,3,5,9};
-        bubbleSort(arr);
-        insertionSort(arr);
-        selectionSort(arr);
+        int arr[] = {1,4,1,3,2,4,3,7};
+        // bubbleSort(arr);
+        // insertionSort(arr);
+        // selectionSort(arr);
+        countingSort(arr);
 
     }
     
